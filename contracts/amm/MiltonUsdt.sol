9a0112bf00e4b5a73a603e0d5bf113a4ed68fade
Mario <mario@ipor.io>
2022-07-11 15:59:41 +0200

    Test merge

--- contracts/amm/MiltonUsdt.sol
+++ contracts/amm/MiltonUsdt.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.15;
 
 import "./Milton.sol";
 
+/// @title Dedicated Milton for USDT asset.
 contract MiltonUsdt is Milton {
     function _getDecimals() internal pure virtual override returns (uint256) {
         return 6;
