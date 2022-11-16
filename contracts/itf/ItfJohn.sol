4365e444f5b2a249d588581538c0f89d81b06b26
Mario <mario@ipor.io>
2022-11-16 13:58:11 +0100

    IL-1867 refactor folder john to mining.

--- contracts/itf/ItfJohn.sol
+++ contracts/itf/ItfJohn.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.16;
 
-import "../john/John.sol";
+import "../mining/John.sol";
 
 contract ItfJohn is John {
     function setPowerIpor(address powerIpor) external {
--- contracts/itf/tests/LiquidityRewardsAgent.sol
+++ contracts/itf/tests/LiquidityRewardsAgent.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.16;
 
-import "../../john/John.sol";
+import "../../mining/John.sol";
 import "../../tokens/PowerIpor.sol";
 
 contract LiquidityRewardsAgent {
