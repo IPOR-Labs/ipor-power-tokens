d82583e16971cb1bc30f88e44303875bf681a10b
Piotr Rzonsowski <pete@ipor.io>
2022-11-24 18:46:12 +0100

    remove

--- contracts/mining/JohnInternal.sol
+++ contracts/mining/JohnInternal.sol
@@ -17,8 +17,6 @@ import "../interfaces/IPowerIporInternal.sol";
 import "../security/IporOwnableUpgradeable.sol";
 import "../libraries/math/MiningCalculation.sol";
 import "../tokens/IporToken.sol";
-import "abdk-libraries-solidity/ABDKMathQuad.sol";
-import "hardhat/console.sol";
 
 abstract contract JohnInternal is
     Initializable,
@@ -463,7 +461,7 @@ abstract contract JohnInternal is
     /// @dev 0.5 = ABDKMathQuad.div(ABDKMathQuad.fromUInt(5), ABDKMathQuad.fromUInt(10))
     /// @dev Notice! uint256 value before calculation has following constraints: 0.5 <= Horizontal Shift <= 10^3
     /// @return horizontal shift - value represented in bytes16, quadrupe precision, 128 bits, takes into consideration 18 decimals
-    function _getHorizontalShift() internal view virtual returns (bytes16) {
+    function _getHorizontalShift() internal pure virtual returns (bytes16) {
         return 0x3ffe0000000000000000000000000000;
     }
 
@@ -472,7 +470,7 @@ abstract contract JohnInternal is
     /// @dev 1.4 = ABDKMathQuad.div(ABDKMathQuad.fromUInt(14), ABDKMathQuad.fromUInt(10))
     /// @dev Notice! uint256 value before calculation has following constraints: 10^(-4) <= Vertival Shift <= 3
     /// @return vertical shift - value represented in bytes16, quadrupe precision, 128 bits, takes into consideration 18 decimals
-    function _getVerticalShift() internal view virtual returns (bytes16) {
+    function _getVerticalShift() internal pure virtual returns (bytes16) {
         return 0x3fff6666666666666666666666666666;
     }
 
