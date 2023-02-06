a9febc8c6da500a307e81d8e25d3ea668e2bb5b1
Piotr Rzonsowski <pete@ipor.io>
2023-02-06 13:19:22 +0100

    add PowerToken and maining setup in foundry tests

--- test/ExampleTest.t.sol
+++ test/ExampleTest.t.sol
@@ -2,12 +2,11 @@
 pragma solidity 0.8.17;
 
 import "forge-std/Test.sol";
+import "./PowerTokensSetup.sol";
 
 contract ExampleTest is Test {
-    function testExample () public {
-
+    function testExample() public {
+        PowerTokensSetup setup = new PowerTokensSetup(address(this));
         assertTrue(true);
     }
-
-
 }
