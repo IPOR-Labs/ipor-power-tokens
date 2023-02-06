ad5f9ffc6085d1381232982782e1721a2bd2edb4
Piotr Rzonsowski <94194924+pete-ipor@users.noreply.github.com>
2023-02-06 12:13:33 +0100

    Feature/il 2431 add foundry (#105)
    * IL-2431 add foundry tests setup

---
+++ test/ExampleTest.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.17;
+
+import "forge-std/Test.sol";
+
+contract ExampleTest is Test {
+    function testExample () public {
+
+        assertTrue(true);
+    }
+
+
+}
