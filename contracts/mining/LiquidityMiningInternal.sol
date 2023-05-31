c7ecf4248f932e3fc92e4018308781f19506550c
Mario <mario@ipor.io>
2023-05-31 22:03:15 +0200

    IL-2955 remove Dx

--- contracts/mining/LiquidityMiningInternal.sol
+++ contracts/mining/LiquidityMiningInternal.sol
@@ -8,7 +8,6 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "../libraries/errors/Errors.sol";
 import "../libraries/math/MiningCalculation.sol";
-import "../libraries/Constants.sol";
 import "../interfaces/types/LiquidityMiningTypes.sol";
 import "../interfaces/ILiquidityMiningInternal.sol";
 import "../interfaces/IStakedToken.sol";
