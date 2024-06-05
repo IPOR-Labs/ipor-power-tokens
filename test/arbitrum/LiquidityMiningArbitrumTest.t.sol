a504e04389c5891e9100e79d529c63ee91069450
Piotr Rzonsowski <94194924+pete-ipor@users.noreply.github.com>
2024-06-05 12:13:01 +0200

    fix (#161)
    LiquidityMining lpusdc fix

--- test/arbitrum/LiquidityMiningArbitrumTest.t.sol
+++ test/arbitrum/LiquidityMiningArbitrumTest.t.sol
@@ -9,25 +9,31 @@ import "../../contracts/interfaces/types/LiquidityMiningTypes.sol";
 
 contract LiquidityMiningArbitrumTest is Test {
     address public constant wstEth = 0x5979D7b546E38E414F7E9822514be443A4800529;
+    address public constant usdc = 0xaf88d065e77c8cC2239327C5EDb3A432268e5831;
     address public constant ipor = 0x34229B3f16fBCDfA8d8d9d17C0852F9496f4C7BB;
     address public constant ethUsdOracle = 0x639Fe6ab55C921f74e7fac1ee960C0B6293ba612;
     address public constant wstEthStEthOracle = 0xB1552C5e96B312d0Bf8b554186F846C40614a540;
 
     address public lpWstEth;
+    address public lpUsdc;
     LiquidityMiningArbitrum public liquidityMining;
 
     function setUp() public {
         vm.createSelectFork(vm.envString("ARBITRUM_PROVIDER_URL"), 18983640);
         lpWstEth = address(new MockLpToken("lpWstEth", "lpWstEth", wstEth));
+        lpUsdc = address(new MockLpToken("lpUsdc", "lpUsdc", wstEth));
         MockLpToken(lpWstEth).setJoseph(address(this));
+        MockLpToken(lpUsdc).setJoseph(address(this));
         LiquidityMiningArbitrum liquidityMiningImpl = new LiquidityMiningArbitrum(
             address(this),
             ethUsdOracle,
-            wstEthStEthOracle
+            wstEthStEthOracle,
+            lpWstEth
         );
 
-        address[] memory lpTokewns = new address[](1);
+        address[] memory lpTokewns = new address[](2);
         lpTokewns[0] = lpWstEth;
+        lpTokewns[1] = lpUsdc;
 
         ERC1967Proxy proxy = new ERC1967Proxy(
             address(liquidityMiningImpl),
@@ -37,7 +43,7 @@ contract LiquidityMiningArbitrumTest is Test {
         liquidityMining = LiquidityMiningArbitrum(address(proxy));
     }
 
-    function testT() external {
+    function testOracles() external {
         // given
         // answerEthUsd 264703534116
         // answerWstEthStEth 1153222004578747471
@@ -77,6 +83,50 @@ contract LiquidityMiningArbitrumTest is Test {
             indicators.delegatedPwTokenBalance,
             100e18,
             "delegatedPwTokenBalance should be 100e18"
+        );
+    }
+
+    function testShouldCalculateDifferentValueForWstEthAndUsdc() external {
+        //        wstEth
+        MockLpToken(lpWstEth).mint(address(this), 100e18);
+        LiquidityMiningTypes.UpdateLpToken[]
+            memory updateLpToken = new LiquidityMiningTypes.UpdateLpToken[](1);
+
+        updateLpToken[0] = LiquidityMiningTypes.UpdateLpToken(address(this), lpWstEth, 10e18);
+        address[] memory lpTokewns = new address[](1);
+
+        LiquidityMiningTypes.UpdatePwToken[]
+            memory updatePwTokens = new LiquidityMiningTypes.UpdatePwToken[](1);
+        updatePwTokens[0] = LiquidityMiningTypes.UpdatePwToken(address(this), lpWstEth, 100e18);
+
+        liquidityMining.addPwTokensInternal(updatePwTokens);
+        liquidityMining.addLpTokensInternal(updateLpToken);
+
+        //        usdc
+        MockLpToken(lpUsdc).mint(address(this), 100e18);
+        updateLpToken[0] = LiquidityMiningTypes.UpdateLpToken(address(this), lpUsdc, 10e18);
+        updatePwTokens[0] = LiquidityMiningTypes.UpdatePwToken(address(this), lpUsdc, 100e18);
+
+        liquidityMining.addPwTokensInternal(updatePwTokens);
+        liquidityMining.addLpTokensInternal(updateLpToken);
+
+        lpTokewns[0] = lpWstEth;
+        LiquidityMiningTypes.AccountIndicatorsResult[] memory indicatorsListWstEth = liquidityMining
+            .getAccountIndicators(address(this), lpTokewns);
+
+        lpTokewns[0] = lpUsdc;
+        LiquidityMiningTypes.AccountIndicatorsResult[] memory indicatorsListUsdc = liquidityMining
+            .getAccountIndicators(address(this), lpTokewns);
+
+        assertEq(
+            indicatorsListWstEth[0].indicators.powerUp,
+            227236277435590833,
+            "powerUp should be 227236277435590833"
+        );
+        assertEq(
+            indicatorsListUsdc[0].indicators.powerUp,
+            5535159583281635767,
+            "powerUp should be 5535159583281635767"
         );
     }
 }
