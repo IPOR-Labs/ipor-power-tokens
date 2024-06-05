fe1a78ae1554d137978fce36958fded4998deb42
Piotr Rzonsowski <pete@ipor.io>
2024-06-05 12:04:42 +0200

    fix

--- contracts/mining/LiquidityMiningArbitrum.sol
+++ contracts/mining/LiquidityMiningArbitrum.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.20;
 
 import "./LiquidityMining.sol";
 import "../interfaces/AggregatorV3Interface.sol";
+import "./CalculateWeightedLpTokenBalanceArbitrum.sol";
 
 /// @title Smart contract responsible for distribution of Power Token rewards across accounts contributing to Liquidity Mining
 /// by staking lpTokens and / or delegating Power Tokens.
@@ -11,14 +12,17 @@ contract LiquidityMiningArbitrum is LiquidityMining {
 
     address internal immutable ethUsdOracle;
     address internal immutable wstEthStEthExchangeRateOracle;
+    address internal immutable lpwstEth;
 
     constructor(
         address routerAddress,
         address ethUsdOracleInput,
-        address wstEthStEthExchangeRateOracleInput
+        address wstEthStEthExchangeRateOracleInput,
+        address lpwstEthInput
     ) LiquidityMining(routerAddress) {
         ethUsdOracle = ethUsdOracleInput;
         wstEthStEthExchangeRateOracle = wstEthStEthExchangeRateOracleInput;
+        lpwstEth = lpwstEthInput;
         _disableInitializers();
     }
 
@@ -26,20 +30,17 @@ contract LiquidityMiningArbitrum is LiquidityMining {
     /// @param lpTokenBalance The balance of lp tokens.
     /// @return uint256 The weighted balance of PW tokens.
     function _calculateWeightedLpTokenBalance(
-        address,
+        address lpToken,
         uint256 lpTokenBalance
     ) internal view override returns (uint256) {
-        // @dev returned value has 8 decimal, on arbitrum 0x639Fe6ab55C921f74e7fac1ee960C0B6293ba612
-        (, int256 answerEthUsd, , , ) = AggregatorV3Interface(ethUsdOracle).latestRoundData();
-        // @dev returned value has 18 decimal, on arbitrum 0xe59eba0d492ca53c6f46015eea00517f2707dc77, not avable on mainnet
-        (, int256 answerWstEthStEth, , , ) = AggregatorV3Interface(wstEthStEthExchangeRateOracle)
-            .latestRoundData();
-
         return
-            MathOperation.division(
-                lpTokenBalance * answerEthUsd.toUint256() * answerWstEthStEth.toUint256(),
-                1e26 // 18 + 8 + 18 - 26 = 18
-            );
+            CalculateWeightedLpTokenBalanceArbitrum._calculateWeightedLpTokenBalance({
+                lpToken_: lpToken,
+                lpTokenBalance_: lpTokenBalance,
+                ethUsdOracle_: ethUsdOracle,
+                lpwstEth_: lpwstEth,
+                wstEthStEthExchangeRateOracle_: wstEthStEthExchangeRateOracle
+            });
     }
 
     function getConfiguration() external view returns (address, address) {
